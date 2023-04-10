ec0e7f8fa72c0e4ecac78f7552f6eb97307ea5c2
Quintanilla <jose.quintanilla@accenture.com>
2023-04-10 10:45:56 -0400

    init de proyecto java

---
+++ src/main/java/cl/ejemplo/tbkdemo/TbkdemoApplication.java
@@ -0,0 +1,13 @@
+package cl.ejemplo.tbkdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbkdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TbkdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/ejemplo/tbkdemo/TbkdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.ejemplo.tbkdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TbkdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
